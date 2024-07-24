700462a8e5fc2fbf86f7d2d6e08070dd1b3e653e
hadooni <hayoung7259@gmail.com>
2024-07-24 12:58:21 +0900

    fix 객체 안 속성 추가하는 함수 수정

--- 240724/solution4.js
+++ 240724/solution4.js
@@ -7,21 +7,20 @@ const lion = {
   대사: '하쿠나마타타',
 };
 
-// 나이를 추가하는 함수입니다.
-// function incrementAge(lion) {
-//   lion.나이 = lion.나이 + 1;
-// }
-// // 함수 실행 이후 사자의 현재 나이를 출력합니다.
-// incrementAge(lion);
-// console.log(lion);
 
+// 나이를 추가하는 함수입니다.
+function incrementAge(lion) {
+  lion.나이 = lion.나이 + 1;
+}
+// 함수 실행 이후 사자의 현재 나이를 출력합니다.
+incrementAge(lion);
+console.log(lion);
 
 
 // 객체안에 속성 추가하는 함수 만들기
-
 function addProperty(inputLion, a, b) {
-    inputLion.a = 'b';
+  inputLion[a] = b;
 }
 
-addProperty(lion, 다리, '4개');
+addProperty(lion, '다리', '4개');
 console.log(lion);
